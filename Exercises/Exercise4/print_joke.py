2650f8ceb843ed53dfed0949bfbaf8f75c794697
Rufshod <alexander@11a.se>
2023-08-18 11:20:05 +0200

    changed folder from pychache to exercise4

---
+++ Exercises/Exercise4/print_joke.py
@@ -0,0 +1,33 @@
+import pyjokes
+
+reactions = [
+    "Hilarious!",
+    "Oh, the humanity!",
+    "You've cracked the code!",
+    "That's comedy gold!",
+    "My sides are splitting!",
+    "Mind = blown!",
+    "Cue the laugh track!",
+    "I'm dying of laughter!",
+    "That's so bad, it's good!",
+    "*Insert uncontrollable laughter here*",
+]
+
+
+def get_random_reaction():
+    import random
+
+    return random.choice(reactions)
+
+
+def print_random_joke_and_reaction():
+    print("\n" + pyjokes.get_joke() + "\n")
+    print(get_random_reaction() + "\n")
+
+
+if __name__ == "__main__":
+    print_random_joke_and_reaction()
+# Testing
+# $ python print_joke.py
+
+# $ python print_joke.py
